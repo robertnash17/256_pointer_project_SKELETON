2f5661ffb65b770c299df1b85db7db4b2f31f76b
keith perkins <kperkins411@gmal.com>
2020-11-16 21:25:44 -0500

    Added a hint to create_list

--- includes/memorymanager.h
+++ includes/memorymanager.h
@@ -12,20 +12,20 @@
 /**
  * create a linked list of  structs.
  * NOTE: be sure you do not leak memory if you call this function twice!
- *       IOW deallocate any exixting list before reallocating
+ *       IOW deallocate any existing list before reallocating
  *
  * \param total_memory: the maximum memory to use
  * \return if a positive number, then its the number of structs created
  *         if 0, then there is not enough memory to allocate a struct
- * 		   MEM_ALREADY_ALLOCATED  if memory has already been allocated, this
- * 		   						  is indicated by total_allocated>0. (BTW why use
- * 		   						  total_allocated instead of list_data==0?)
+ * 		   if MEM_ALREADY_ALLOCATED  if memory has already been allocated by this function.
+ * 		   						     You can tell if memory has already been allocated
+ * 		   						     by whether list_data points to null or not.
  */
 int create_list(int total_memory);
 
 /**
  * deallocate the list of structs, easiest way is to start at the beginning
- * and delete as you go, dont forget to set list_data to null when done
+ * and delete as you go, don't forget to set list_data to null when done
  *
  * \return SUCCESS if memory deallocated
  *         NO_STRUCTS_TO_DEALLOCATE if no memory allocated
